f2595890eb215e24917dbe7512e0798341a4d015
Themanual22 <djamosry22@gmail.com>
2022-01-31 19:06:41 -0500

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+// currentUser()
+// function currentUser(){
+// let currentUser = 'Bella'
+// let welcomeMessage = 'Welcome to Flatbook!'
+// let currentUser = 'welcomeMessage'
+
+// let excitedWelcomeMessage = 'WELCOME TO FLATBOOK!';
+// let shortGreeting = 'Welcome,B!'
+// }
+// excitedWelcomeMessage()
+// shortGreeting()
+
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
