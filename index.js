59c648ce106b9f99429b346f75f58521b604a918
Themanual22 <djamosry22@gmail.com>
2022-06-12 23:49:33 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// currentUser()
-// function currentUser(){
-// let currentUser = 'Bella'
-// let welcomeMessage = 'Welcome to Flatbook!'
-// let currentUser = 'welcomeMessage'
-
-// let excitedWelcomeMessage = 'WELCOME TO FLATBOOK!';
-// let shortGreeting = 'Welcome,B!'
-// }
-// excitedWelcomeMessage()
-// shortGreeting()
-
-
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
